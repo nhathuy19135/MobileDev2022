db4f044dda41c17fe684e0eb559efd0ee770485b
unknown <phamchaugiatu123@gmail.com>
2022-10-05 08:29:19 +0700

    Anh add feature login

--- app/src/main/java/com/example/mobiledev2022/MainActivity.java
+++ app/src/main/java/com/example/mobiledev2022/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mobiledev2022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonSignIn;
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        textView = findViewById(R.id.textView);
+        firebaseAuth = FirebaseAuth.getInstance();
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginGoogle();
+            }
+        });
+
+    }
+    private void loginGoogle() {
+        Intent intent = gsc.getSignInIntent();
+        startActivity(intent);
+        textView.setText(intent.toString());
+        Log.d("asd",textView.toString());
     }
 }
